52cd3b22648cfff903670d6669f47fb54290aed1
sofi <nozdryuhinas@mail.ru>
2024-04-12 11:38:21 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace WindowsFormsApp10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(109, 140);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(287, 277);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(433, 140);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(287, 277);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(109, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Массив из чисел";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(433, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(287, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Отсортированный массив";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(109, 436);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(287, 33);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Отсортировать";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "По возрастанию",
+            "По убыванию"});
+            this.comboBox1.Location = new System.Drawing.Point(433, 436);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(287, 30);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1313, 738);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp10
+{
+    public partial class Form1 : Form
+    {
+        // Номер сортировки по возрастанию (индекс в списке)
+        const int ASCENDING_SORT = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = ASCENDING_SORT;
+        }
+        // Делегат для сравнивания двух чисел
+        private delegate bool CompareNumbers(double num1, double num2);
+        private static bool IsGreater(double num1, double num2)
+        {
+            return num1 > num2;
+        }private static bool IsLess(double num1, double num2)
+        {
+            return num1 < num2;
+        }
+        // Сортировка массива методом вставки
+        // Если compare ссылается на isGreater, то сортировка по возрастанию
+        // Если compare ссылается на isLess, то сортировка по убыванию
+        private void SortNumberArray(double[] array, CompareNumbers compare)
+        {
+            for (int i = 1; i < array.Length; i++)
+            { 
+                double key = array[i]; 
+                int j = i - 1;
+                while (j >= 0 && compare(array[j], key)) 
+                {
+                    array[j + 1] = array[j]; 
+                    j -= 1; 
+                } 
+                array[j + 1] = key; 
+            }
+        }
+        // По нажатию кнопки для сортировки введенного массива
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Инициализируем введенный массив
+            string[] tmp = textBox1.Text.Split('\n');
+            double[] array = new double[tmp.Length];
+            for (int i = 0; i < tmp.Length; i++)
+                double.TryParse(tmp[i], out array[i]);
+            // Создаем делегат, который будет определять порядок сортировки
+            CompareNumbers compare;
+            if (comboBox1.SelectedIndex == ASCENDING_SORT)
+                compare = new CompareNumbers(IsGreater);
+            else
+                compare = new CompareNumbers(IsLess);
+            // Вызываем метод сортировки, передавая в его аргументах делегат
+            SortNumberArray(array, compare);
+            // Выводим сортированный массив
+            textBox2.Text = "";
+            foreach (double number in array)
+                textBox2.Text += $"{number}\r\n";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
